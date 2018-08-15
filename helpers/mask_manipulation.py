8f21b0a9d523d7326d34e271465797bcfa69912c
john <john.crousse@allianz.be>
2018-08-15 21:57:34 +0200

    binary_to_mask helper

--- helpers/mask_manipulation.py
+++ helpers/mask_manipulation.py
@@ -54,7 +54,6 @@ def mask_to_binary(rle_mask, dim=(101,101), index_zero=False, col_encoding=True)
 
 def binary_to_mask(binary_mask, index_zero=False, col_encoding=True):
     """
-
     :param binary_mask: array containing the mask with 0/1 values
     :param index_zero: whether the encoding should start at index 0 (True) or not
     :param col_encoding: whether the output encoding should be by column (True) or row
